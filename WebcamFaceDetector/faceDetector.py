8280dc08b3b8e08b892add3eed74c151a19d39f2
SJuras <jurasstpko@gmailcom>
2021-12-31 07:42:17 +0100

    v1.1.1 face detection functionality added.

---
+++ WebcamFaceDetector/faceDetector.py
@@ -0,0 +1,30 @@
+import cv2
+from random import randrange
+
+# detect the cascade, to use it later
+trained_face_data = cv2.CascadeClassifier('./Resources/haarcascade_frontalface_default.xml')
+
+# video capture
+webcam = cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read, frame = webcam.read()
+
+    # change frame to grayscale
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces
+    face_coordinates = trained_face_data.detectMultiScale(frameGray)
+
+    # draw a rectangle around the face
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.putText(frame, "Person", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
+
+    # show frame
+    cv2.imshow("Video Feed", frame)
+    # pauses the termination of the program
+    cv2.waitKey(1)
+
+
+print("Code Execured")
